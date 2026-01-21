334d7ac138d75be8b773bd760d398ebf45f74fba
Zaworov <zavvorov@gmail.com>
2026-01-21 18:41:19 +0100

    Initial commit

---
+++ src/test/java/pl/kayakplanner/app/KayakPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kayakplanner.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayakPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
